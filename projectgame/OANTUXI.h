7dd319b586ac97fcc081e3649b2b8cc17b6ac7eb
Neihhhh <hien13042006@gmail.com>
2025-02-20 00:26:02 +0700

    Add files via upload

---
+++ projectgame/OANTUXI.h
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std;
+
+string computerChoice();
+void printResult(string computerChoose, string playerChoose);
---
+++ projectgame/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include"OANTUXI.h"
+#include"XUCXAC.H"
+using namespace std;
+
+int main(){
+    cout<<rollDice();
+}
